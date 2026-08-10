0cc1b1b7c7f4c8147e2b45a3eb95e808296ece8c
KnaZII <petya15062011@gmail.com>
2026-08-10 18:41:13 +0700

    youtubefix

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ window.AXIS_CONFIG = {
   telegramChannelUrl: "https://t.me/AxisProjectAI",
   demoUrl: "#showcase",
 
-  youtubeVideoIdEn: "https://youtu.be/6rYL6fLL_Go",
-  youtubeVideoIdRu: "https://youtu.be/cgppy7u7UkQ"
+  youtubeVideoIdEn: "6rYL6fLL_Go",
+  youtubeVideoIdRu: "cgppy7u7UkQ"
 };
